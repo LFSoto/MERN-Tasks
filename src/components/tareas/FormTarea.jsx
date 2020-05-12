a35730bb4693c786ff7210748959377f757cb147
LFSoto <soto2197@gmail.com>
2020-05-11 22:21:42 -0600

    Actualizacion acumulativa
    Se realizaron ajustes al agregar un nuevo producto, ya que despues de agregar no le asignada un id lo que no permitia la edicion.

--- src/components/tareas/FormTarea.jsx
+++ src/components/tareas/FormTarea.jsx
@@ -1,25 +1,114 @@
-import React from "react";
+import React, { useContext, useState, useEffect } from "react";
+
+import proyectoContext from "../../context/proyectos/proyectoContext";
+import tareaContext from "../../context/tareas/tareaContext";
 
 const FormTarea = () => {
+  //Extraer si un proyecto esta activo
+  const proyectosContext = useContext(proyectoContext);
+  const { proyecto } = proyectosContext;
+
+  //Extraer funcion de tarea
+  const tareasContext = useContext(tareaContext);
+  const {
+    tareaseleccionada,
+    errortarea,
+    agregarTarea,
+    validarTarea,
+    obtenerTareas,
+    editarTarea,
+    limpiarSeleccionada,
+  } = tareasContext;
+
+  //Effect que detecta si hay una tarea seleccioada para edicion
+  useEffect(() => {
+    if (tareaseleccionada !== null) {
+      guardarTarea(tareaseleccionada);
+    } else {
+      guardarTarea({
+        nombre: "",
+      });
+    }
+  }, [tareaseleccionada]);
+
+  //State del form
+  const [tarea, guardarTarea] = useState({
+    nombre: "",
+  });
+
+  //Extraer el nombre de la tarea
+  const { nombre } = tarea;
+
+  //Si no hay proyecto seleccionado
+  if (!proyecto) return null;
+
+  //Array destructuring para extraer el proyecto seleccionado
+  const [proyectoActual] = proyecto;
+
+  //Leer los valores del form
+  const handleChange = (e) => {
+    guardarTarea({
+      ...tarea,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  //Agregar la nueva tarea al proyecto
+  const onSubmitTarea = (e) => {
+    e.preventDefault();
+
+    //Validar
+    if (nombre.trim() === "") {
+      validarTarea();
+      return;
+    }
+
+    //Revisa si es edicion o nueva tarea
+    if (tareaseleccionada === null) {
+      //Agregar la nueva tarea al state de tareas
+      tarea.proyectoId = proyectoActual.id;
+      tarea.estado = false;
+      agregarTarea(tarea);
+    } else {
+      //Actualizar la tarea existente
+      editarTarea(tarea);
+      //Limpia la tarea seleccionada
+      limpiarSeleccionada();
+    }
+
+    //Obtener y filtar las tareas del proyecto actual
+    obtenerTareas(proyectoActual.id);
+
+    //Reiniciar el form
+    guardarTarea({
+      nombre: "",
+    });
+  };
+
   return (
     <div className="formulario">
-      <form>
+      <form onSubmit={onSubmitTarea}>
         <div className="contenedor-input">
           <input
             type="text"
             className="input-text"
             placeholder="Nombre Tarea"
             name="nombre"
+            value={nombre}
+            onChange={handleChange}
           />
         </div>
         <div className="contenedor-input">
           <input
-            type="submt"
+            type="submit"
             className="btn btn-primario btn-submit btn-block"
-            value="Agregar Tarea"
+            value={tareaseleccionada ? "Editar Tarea" : "Agregar Tarea"}
           />
         </div>
       </form>
+      {errortarea ? (
+        <p className="mensaje error">Debe indicar un nombre para la tarea</p>
+      ) : null}
     </div>
   );
 };
--- src/components/tareas/Tarea.jsx
+++ src/components/tareas/Tarea.jsx
@@ -1,27 +1,80 @@
-import React from "react";
+import React, { useContext } from "react";
+
+import proyectoContext from "../../context/proyectos/proyectoContext";
+import tareaContext from "../../context/tareas/tareaContext";
 
 const Tarea = ({ tarea }) => {
   const { nombre, estado } = tarea;
+
+  //Extraer funcion de tarea
+  const tareasContext = useContext(tareaContext);
+  const {
+    eliminarTarea,
+    obtenerTareas,
+    cambiarEstadoTarea,
+    guardarTareaActual,
+  } = tareasContext;
+
+  //Para obtener el id del proyecto activo
+  const proyectosContext = useContext(proyectoContext);
+  const { proyecto } = proyectosContext;
+
+  //Extraer el proyecto con ARRAY DESTRUCTURING
+  const [proyectoActual] = proyecto;
+
+  //Funcion que se ejecuta cuando el usuario presiona el boton de eliminar tarea.
+  const tareaEliminar = (id) => {
+    eliminarTarea(id);
+    obtenerTareas(proyectoActual.id);
+  };
+
+  //Funcion que modifica el state de las tareas
+  const cambiarEstado = (tarea) => {
+    tarea.estado ? (tarea.estado = false) : (tarea.estado = true);
+
+    cambiarEstadoTarea(tarea);
+  };
+
+  //Funcion que agrega una tarea actual cuando se desea editar
+  const seleccionarTarea = (tarea) => {
+    guardarTareaActual(tarea);
+  };
 
   return (
     <li className="tarea sombra">
       <p>{nombre}</p>
       <div className="estado">
         {estado ? (
-          <button type="button" className="completo">
+          <button
+            type="button"
+            className="completo"
+            onClick={() => cambiarEstado(tarea)}
+          >
             Completo
           </button>
         ) : (
-          <button type="button" className="incompleto">
+          <button
+            type="button"
+            className="incompleto"
+            onClick={() => cambiarEstado(tarea)}
+          >
             Incompleto
           </button>
         )}
       </div>
       <div className="acciones">
-        <button type="button" className="btn btn-primario">
+        <button
+          type="button"
+          className="btn btn-primario"
+          onClick={() => seleccionarTarea(tarea)}
+        >
           Editar
         </button>
-        <button type="button" className="btn btn-secundario">
+        <button
+          type="button"
+          className="btn btn-secundario"
+          onClick={() => tareaEliminar(tarea.id)}
+        >
           Eliminar
         </button>
       </div>
